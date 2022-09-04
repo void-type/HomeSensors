4a9ea079bbd47dcaadd0e0d949f10dd46253ea17
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2022-09-04 01:33:26 -0600

    Monthly averages sql

--- build/sql/_analyze.sql
+++ build/sql/_analyze.sql
@@ -62,12 +62,13 @@ WHERE [CurrentTemperatureLocationId] IS NULL
 -- Average times
 SELECT
   tr.[Time]
-,  td.DeviceModel
+, td.DeviceModel
 FROM [dbo].[TemperatureReadings] tr
   INNER JOIN [dbo].[TemperatureDevices] td ON td.[Id] = tr.[TemperatureDeviceId]
   LEFT JOIN [dbo].[TemperatureLocations] tl ON tl.[Id] = tr.[TemperatureLocationId]
 where td.Id = 7
-AND  Time >= DateAdd(SECOND, -14400, (select Convert(DateTime2, max(time)) from TemperatureReadings))
+  AND Time >= DateAdd(SECOND, -14400, (select Convert(DateTime2, max(time))
+  from TemperatureReadings))
 ORDER BY [Time] ASC
 
 -- $first, $times = Get-Content .\times.csv | % { [DateTimeOffset]::Parse($_) }; $times | % { $temp = $first; $first = $_; ($_ - $temp).TotalSeconds } | measure -Average | select -expand Average
@@ -78,3 +79,18 @@ ORDER BY [Time] ASC
 -- Acurite-986 144.29
 -- Acurite-986 122.68
 -- Acurite-Tower 16.90
+
+-- Monthly averages
+SELECT
+  l.[Name] AS LocationName
+      , ROUND(MIN([TemperatureCelsius]), 2) as MinTemp
+      , ROUND(AVG([TemperatureCelsius]), 2) as AvgTemp
+      , ROUND(MAX([TemperatureCelsius]), 2) as MaxTemp
+      , ROUND(MIN([Humidity]), 2) as MinHumidity
+      , ROUND(AVG([Humidity]), 2) as AvgHumidity
+      , ROUND(MAX([Humidity]), 2) as MaxHumidity
+FROM [HomeSensors].[dbo].[TemperatureReadings] r
+  INNER JOIN TemperatureLocations l ON l.Id = r.TemperatureLocationId
+-- LEFT JOIN TemperatureDevices d ON d.Id = r.TemperatureDeviceId
+WHERE r.[Time] BETWEEN '2022-08-01 00:00:00' AND '2022-08-31 00:00:00'
+GROUP BY l.Name
