a531dc08d77b3d7e633419fe57de6c37760c5f3c
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-03-07 21:50:47 -0700

    Fontawesome CSP

--- src/ClientApp/src/main.ts
+++ src/ClientApp/src/main.ts
@@ -2,16 +2,17 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import 'bootstrap';
 import router from '@/router';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library, config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core';
 import { faClock, faSnowflake, faTemperatureFull } from '@fortawesome/free-solid-svg-icons';
 import SetupCalendar, { DatePicker } from 'v-calendar';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import App from './App.vue';
 
-library.add(faTemperatureFull);
-library.add(faSnowflake);
-library.add(faClock);
+// Prevents inline styling to appease CSP.
+fontAwesomeConfig.autoAddCss = false;
+
+library.add(faTemperatureFull, faSnowflake, faClock);
 
 const app = createApp(App);
 
