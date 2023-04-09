65d0530002dfb1e2b8fea1348b06e70f1b7d1652
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-04-08 22:39:10 -0600

    No longer need appsettings for migration

--- build/dbCreateMigration.ps1
+++ build/dbCreateMigration.ps1
@@ -11,12 +11,6 @@ try {
   Set-Location -Path $projectRoot
   . ./build/buildSettings.ps1
 
-  $settingsFile = "$serviceProjectFolder/appsettings.Development.json"
-
-  if (-not (Test-Path -Path $settingsFile)) {
-    throw "$settingsFile does not exist to get the Connection String from."
-  }
-
   dotnet ef migrations add "$MigrationName" `
     --project "$modelProjectFolder" `
     --startup-project  "$serviceProjectFolder"
