d2dc7b4bce01840c9239076c19b70eb51e2dfcba
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2023-12-25 13:38:42 -0700

    Cleanup script

--- build/sql/_cleanup.sql
+++ build/sql/_cleanup.sql
@@ -1,6 +1,46 @@
 -- Cleanup readings that weren't associated with a location. Cleanup devices that have no readings.
 
-delete from TemperatureReadings where TemperatureLocationId is null
+DELETE FROM TemperatureReadings WHERE TemperatureLocationId IS NULL
 
-delete d FROM [HomeSensors].[dbo].[TemperatureDevices] as d
-where not exists ( select * from TemperatureReadings r where r.TemperatureDeviceId = d.Id  )
+DELETE d FROM [HomeSensors].[dbo].[TemperatureDevices] AS d
+WHERE NOT EXISTS ( SELECT *
+FROM TemperatureReadings r
+WHERE r.TemperatureDeviceId = d.Id  )
+
+
+-- Clone data down from prod to test
+TRUNCATE TABLE HomeSensorsTest.dbo.TemperatureReadings
+DELETE FROM HomeSensorsTest.dbo.TemperatureDevices
+DELETE FROM HomeSensorsTest.dbo.TemperatureLocations
+
+
+SET IDENTITY_INSERT HomeSensorsTest.dbo.TemperatureLocations ON
+GO
+
+INSERT INTO HomeSensorsTest.dbo.TemperatureLocations
+  ([Id]
+  ,[Name]
+  ,[MaxTemperatureLimitCelsius]
+  ,[MinTemperatureLimitCelsius])
+SELECT *
+FROM HomeSensors.dbo.TemperatureLocations
+
+SET IDENTITY_INSERT HomeSensorsTest.dbo.TemperatureLocations OFF
+GO
+
+
+SET IDENTITY_INSERT HomeSensorsTest.dbo.TemperatureDevices ON
+GO
+
+INSERT INTO HomeSensorsTest.dbo.TemperatureDevices
+  ([Id]
+  ,[DeviceModel]
+  ,[DeviceId]
+  ,[DeviceChannel]
+  ,[CurrentTemperatureLocationId]
+  ,[IsRetired])
+SELECT *
+FROM HomeSensors.dbo.TemperatureDevices
+
+SET IDENTITY_INSERT HomeSensorsTest.dbo.TemperatureDevices OFF
+GO
